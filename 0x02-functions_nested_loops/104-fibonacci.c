13b97690b2e9489b3d8c530bb868e84826c4e46a
OkekeChristianChijioke <okeke.christianchijioke@gmail.com>
2022-12-14 04:21:07 -0800

    Write a program that finds and prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+:#include <stdio.h>
 
 /**
  * numLength - returns the length of string
@@ -10,21 +10,21 @@
 int numLength(int num);
 
 '{'
-	int	length = 0;
+	int length = 0;
 
 	if (!num)
 	{
-		return	(1);
+		return (1);
 
 	}
 
-	while	(num)
+	while (num)
 	{
 		num = num / 10;
 		length += 1;
 	}
 
-	return	(length);
+	return (length);
 '}'
 
 /**
@@ -39,13 +39,13 @@ int main(void)
 	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f10 = 0, f20 = 0, tmpo = 0;
 	short int i = 1, initial0s;
 
-	while	(i <= 98)
+	while (i <= 98)
 	{
 		if (f10 > 0)
 			printf("%lu", f10);
 		initial0s = numLength(mx) - 1 - numLength(f1);
 
-		while	(f10 > 0 && initial0s > 0)
+		while (f10 > 0 && initial0s > 0)
 		{
 			printf("%i", 0);
 			initial0s--;
